c404e24acc3d4c213f85f5b868ec1610d99618bf
Surk7301 <sukr7581@gmail.com>
2026-04-15 07:09:47 +0530

    added min and max elment in an array

--- lecture14_Arrays/src/Main.java
+++ lecture14_Arrays/src/Main.java
@@ -16,19 +16,29 @@ public class Main {
 //        System.out.println("Value at 0 index: " +brr[3]);
 
         int n = brr.length; // length of array
-        int sum =0 ;
-        for(int i =0; i<=n-1; i++){
-            int value = brr[i];
-            sum += value;
+//        int sum =0 ;
+//        for(int i =0; i<=n-1; i++){
+//            int value = brr[i];
+//            sum += value;
+//
+//        }
+//        System.out.println("Sum of array is: " + sum);
 
-        }
-        System.out.println("Sum of array is: " + sum);
+//        int multiply =1;
+//        for(int i=0; i<=n-1; i++){
+//            int value = brr[i];
+//            multiply *= value;
+//        }
+//        System.out.println("Multiply of array is: " + multiply);
+
+        // Maximum element in array
+
 //        for(int val: brr){
 //            System.out.println(val);
 //        }
 
 //        int arr2[]= new int[5];
-        //loop
+
 //        for(int i=0; i<arr2.length; i++){
 //            System.out.println("Enter value for index " + i);
 //              arr2[i] = scan.nextInt();
@@ -37,6 +47,25 @@ public class Main {
 //            System.out.println("Array contains: ");
 //            System.out.println(val1);
 //        }
+
+        int arrr[] = {3,2,-5,21,10};
+        int size = arr.length;
+        int maxValue = arrr[0];
+        for(int i=0; i<=n-1; i++){
+            if(arrr[i]>maxValue){
+                maxValue = arrr[i];
+            }
+        }
+        System.out.println("Maximum value in array is: " + maxValue);
+
+        int minValue = arrr[0];
+        for(int i=0;i<n-1; n++){
+            if(arrr[i]<minValue){
+                minValue=arrr[i];
+            }
+        }
+        System.out.println("Minimum value in array is: " + minValue);
+
 
     }
 }
